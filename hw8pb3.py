cc2b24d0eb225e527674e8d97072d63ff9837e77
Christopher Rabotin <christopher.rabotin@gmail.com>
2017-03-10 17:52:19 -0700

    I find the exact x which corresponds to Y_noisy but that doesn't help.

--- hw8pb3.py
+++ hw8pb3.py
@@ -4,7 +4,7 @@ import cvxpy as cvx
 import matplotlib.pyplot as plt
 
 def L(X):
-    n1, n2 = X.size
+    n1, n2 = 15, 15
     In1 = np.identity(n1)
     In2 = np.identity(n2)
     Dn1 = -1*np.identity(n1)
@@ -20,14 +20,13 @@ def L(X):
 
     Lh = np.kron(Dn2, In2)
     Lv = np.kron(In1, Dn1)
-    return (Lh, Lv)
+    return np.vstack((Lh, Lv))
 
 def g(y):
     gval = []
     for yi in y:
-        gval.append(np.sqrt(yi[0]**2+yi[1]**1))
-    rtn = sum(gval)
-    return rtn
+        gval.append(np.sqrt(yi[0]**2+yi[1]**2))
+    return np.sum(np.array(gval))
 
 if __name__ == '__main__':
     import h5py
@@ -39,21 +38,42 @@ if __name__ == '__main__':
         Y[k] = np.array(v)
     Y = Y['Y']
 
+    # Save loaded image to check
+    plt.imshow(Y, vmin=0, vmax=1)
+    plt.draw()
+    plt.savefig('hw8pb3-normal.png')
+
+
     r, c = 150, 150
     Y_noisy = np.zeros([r, c])
+    noise_points = 0
     for i in range(r):
         for j in range(c):
             Y_noisy[i][j] = Y[i][j]
             if np.random.uniform(0, 1) <= 0.1:
+                 noise_points += 1
                  Y_noisy[i][j] += np.random.uniform(0, 1)
 
+    print('added {} noise points (should be {})'.format(noise_points, r*c*0.1))
     Y_noisy_snorm = 0.5*np.sum(np.linalg.norm(np.stack(Y_noisy))**2)
+    # Save noisy image
+    plt.clf()
+    plt.draw()
+    plt.imshow(Y_noisy, vmin=0, vmax=1)
+    plt.draw()
+    plt.savefig('hw8pb3-noisy.png')
     tau = 0.25*Y_noisy_snorm
-    import pdb; pdb.set_trace()
+    tauV = cvx.Variable(1)
 
-    x = cvx.Variable(150)
-    func = 0.5*cvx.sum_entries(cvx.norm2(cvx.hstack(Y_noisy - x))**2)
-    prob = cvx.Problem(cvx.Minimize(func), constraints=[g(L(x)) <= tau])
+    x = cvx.Variable(r, c)
+    z = cvx.Variable(450, 225)
+
+    func = (0.5*cvx.sum_entries(cvx.norm2(Y_noisy - x)**2))
+    prob = cvx.Problem(cvx.Minimize(func), constraints=[L(x) == z, g(z) <= tauV, tauV == tau, 0 <= x, x <= 1])
     prob.solve(verbose=True)
-    rslt = prob.variables()[0].value
-    print('result = ', rslt)
+    # Save the result.
+    plt.clf()
+    plt.draw()
+    plt.imshow(Y_noisy - x, vmin=0, vmax=1)
+    plt.draw()
+    plt.savefig('hw8pb3-fixed.png')
